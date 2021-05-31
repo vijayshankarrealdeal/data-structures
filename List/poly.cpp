6d948e19de68ae95088d81085d9bbd2bb8c34506
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-05-31 14:32:03 +0530

    plo

--- List/poly.cpp
+++ List/poly.cpp
@@ -19,13 +19,26 @@ class Polynomial{
                 head_ = tempnode_ = newnode_;
             }else{
                 tempnode_->next_ = newnode_;
-                tempnode_ = newnode_;
+                tempnode_ = tempnode_->next_;
             }
+        }
+        void transverse_()
+        {
+            tempnode_ = head_;
+            while (tempnode_!=NULL)
+            {
+                std::cout<<tempnode_->coff_<<"X^"<<tempnode_->exp_<<"\t";
+            }
+            std::cout<<"\n";
         }
         
 };
 
 int main()
 {
-    
+    Polynomial p;
+    p.addPolynomial(3,14);
+    p.addPolynomial(2,8);
+    p.addPolynomial(1,0);
+    p.transverse_();
 }
