312de9a6db9752b0c4faab4ff7c8305fe86b88af
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-05-31 14:50:25 +0530

    sdsd

--- List/poly.cpp
+++ List/poly.cpp
@@ -11,9 +11,12 @@ class Polynomial{
         struct poly *head_ =NULL;
         struct poly *newnode_,*tempnode_;
     public:
-        void addPolynomial(int coff_,int exp_)
+        void addPolynomial(int coffT_,int expT_)
         {
             newnode_ = new poly;
+            newnode_->coff_ = coffT_;
+            newnode_->exp_ = expT_;
+            newnode_->next_ = NULL;
             if(head_ == NULL)
             {
                 head_ = tempnode_ = newnode_;
@@ -28,17 +31,32 @@ class Polynomial{
             while (tempnode_!=NULL)
             {
                 std::cout<<tempnode_->coff_<<"X^"<<tempnode_->exp_<<"\t";
+                tempnode_ = tempnode_->next_;
             }
             std::cout<<"\n";
+        }
+
+        void addploy_(Polynomial a,Polynomial b)
+        {
+           struct poly *ptr1 = new poly;
+           struct poly *ptr2 = new poly;
+           ptr1 = a.head_;
+           ptr2 = b.head_;
+            std::cout<<ptr1->coff_;
         }
         
 };
 
 int main()
 {
-    Polynomial p;
-    p.addPolynomial(3,14);
-    p.addPolynomial(2,8);
-    p.addPolynomial(1,0);
-    p.transverse_();
+    Polynomial a,b;
+    a.addPolynomial(3,14);
+    a.addPolynomial(2,8);
+    a.addPolynomial(1,0);
+    a.transverse_();
+    std::cout<<"\n";
+    b.addPolynomial(8,14);
+    b.addPolynomial(-3,10);
+    b.addPolynomial(10,6);
+    a.addploy_(a,b);
 }
