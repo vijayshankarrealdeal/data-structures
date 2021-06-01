a88d089aacc16f0b5f62076e0d4dc37467ee119e
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-06-01 20:12:00 +0530

    saddf

--- binaryTree/bT.cpp
+++ binaryTree/bT.cpp
@@ -3,6 +3,21 @@
 struct node
 {
     int data_;
-    struct node *left;
-    struct node *right;
+    struct node *left_;
+    struct node *right_;
+};
+
+class BinaryTree{
+    private:
+        struct node* head_ = NULL;
+        struct node *newnode_,*tempnode_;
+    public:
+        struct node *createNode(int key_)
+        {
+            newnode_ = new node;
+            newnode_->data_ = key_;
+            newnode_->left_ = NULL;
+            newnode_->right_ = NULL;
+        }
+
 };
